a2c5f1faccde7442d3f74146dd17e5fb7a008f1c
codenotes <gbrill@infusion.com>
2015-07-14 17:58:08 -0400

    added client for beacon

---
+++ opti_triangulator/beaconLocationClient/beaconLocationClient.cpp
@@ -0,0 +1,14 @@
+// beaconLocationClient.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+#pragma comment(lib, "ROSIndigoDLL.lib")
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
