a23f5e7bd9f2d1c24d67d9385de6193ab94eba7f
codenotes <gbrill@infusion.com>
2015-07-14 22:50:18 -0400

    filled out client a little more

--- opti_triangulator/beaconLocationClient/beaconLocationClient.cpp
+++ opti_triangulator/beaconLocationClient/beaconLocationClient.cpp
@@ -2,13 +2,35 @@
 //
 
 #include "stdafx.h"
-
+#include "triangulator/beaconSettings.h"
 
 #pragma comment(lib, "ROSIndigoDLL.lib")
 
+using namespace triangulator;
+
+
+void chatterCallback(const triangulator::beaconSettings & msg)
+{
+	
+	
+//	ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	ros::init(argc, argv, "beaconTracker");
+
+	/**
+	* NodeHandle is the main access point to communications with the ROS system.
+	* The first NodeHandle constructed will fully initialize this node, and the last
+	* NodeHandle destructed will close down the node.
+	*/
+	ros::NodeHandle n;
+	ros::Subscriber sub = n.subscribe("beaconPublish", 1000, chatterCallback);
+
+	ros::spin();
+
 	return 0;
 }
 
