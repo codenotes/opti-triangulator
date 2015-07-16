f5e93f7ea901a83bb4f4ad98700b370edf5f445f
codenotes <gbrill@infusion.com>
2015-07-16 19:09:58 -0400

    brought all up 2015 including the mocap actionlib.  Client works talking to remote host via service.

--- opti_triangulator/beaconLocationClient/beaconLocationClient.cpp
+++ opti_triangulator/beaconLocationClient/beaconLocationClient.cpp
@@ -29,21 +29,52 @@ enum TriangulatorDSCommands { GET_LOCALIZATION = 6, GET_VOLTAGE = 3, SET_ENV_LIM
 int _tmain(int argc, _TCHAR* argv[])
 {
 
+	std::vector<float> envLimits;
 
 	ros::init(argc, argv, "beaconTracker");
 
 
 	ros::NodeHandle n;
 	//ros::Subscriber sub = n.subscribe("beaconPublish", 1000, beaconCallback);
-	std::string topic = "beaconPublish";
-	triangulator::beaconSettingsConstPtr p=ros::topic::waitForMessage<triangulator::beaconSettings>(topic);
-	//
-	ros::ServiceClient client = n.serviceClient<triangulator::triangulatorCommand>("triangulatorCmd");//this is important as it is the name of the service we are looking for
-	triangulator::triangulatorCommand srv;
+	//std::string topic = "beaconPublish";
+
+	n.getParam("envLimits", envLimits);
+
+	printf("Env Limits: XMinus:%.4f XPlus:%.4f YMinus:%.4f YPlus:%.4f ZMinus:%.4f ZPlus:%.4f \n",
+		envLimits[0],
+		envLimits[1],
+		envLimits[2],
+		envLimits[3],
+		envLimits[4],
+		envLimits[5]
+		);
 
 	
+	triangulator::beaconSettingsConstPtr p=		
+		ros::topic::waitForMessage< triangulator::beaconSettings>("beaconPublish");
+	////
+	//
+
+	//
+	for (triangulator::beaconSetting b : p->beaconValues)
+	{
+
+		printf("%s\t\t%.5f\t\t%.5f\t\t%.5f\n", b.beaconName.c_str(), b.X, b.Y, b.Z);
+
+
+	}
+
+	
+	ros::ServiceClient client = n.serviceClient<triangulator::triangulatorCommand>("triangulatorCmd");//this is important as it is the name of the service we are looking for
+	triangulator::triangulatorCommand srv;
 	srv.request.triCommand.bSettings.beaconValues = p->beaconValues;
 	srv.request.triCommand.commandID = TriangulatorDSCommands::SET_BEACON_LIST;
+	
+	
+	for (int i = 0; i < envLimits.size(); i++)
+		srv.request.triCommand.envLimits[i] = envLimits[i];
+
+	
 
 	if (client.call(srv))
 	{
@@ -54,7 +85,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	}
 
-	ros::spin();
+	ros::spinOnce();
 
 	return 0;
 }
