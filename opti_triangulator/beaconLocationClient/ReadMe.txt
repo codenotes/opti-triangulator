89f84c4813b2d22bcd5133578b3e84b2bfd0b529
codenotes <gbrill@infusion.com>
2015-07-16 19:09:58 -0400

    brought all up 2015 including the mocap actionlib.  Client works talking to remote host via service.

--- opti_triangulator/beaconLocationClient/ReadMe.txt
+++ opti_triangulator/beaconLocationClient/ReadMe.txt
@@ -1,3 +1,5 @@
 This application listens for beaconSettings messages coming from the mocap. When it gets one, it calls the 
 beagletriangulater's triangleCommand service and tells it the details about the beacons. It commands it to set up its 
 localization based on thost beacon arrangements. 
+
+NOTE: get asserition error on line 52 dgbdel.cpp when using 2015 platform.  2013 is ok.  Not sure what issue might be, so dropped back down. 
--- opti_triangulator/opti_triangulator/actionlibClient/NodeTemplateUnmanaged.cpp
+++ opti_triangulator/opti_triangulator/actionlibClient/NodeTemplateUnmanaged.cpp
@@ -43,7 +43,16 @@
 #include "triangulator/beaconSettings.h"
 
 //const double tf::Transformer::DEFAULT_CACHE_TIME = 10.0;
-#pragma comment(lib, "C:\\Users\\gbrill\\Source\\Repos\\ROSIndigo\\ROSIndigoDLL\\Debug\\ROSIndigoDLL.lib")
+#pragma comment(lib, "ROSIndigoDLL.lib")
+#pragma comment(lib, "boost_system-vc140-mt-gd-1_58.lib")
+#pragma comment(lib, "boost_thread-vc140-mt-gd-1_58.lib")
+//#pragma comment(lib,"boost_signals-vc140-mt-gd-1_58.lib")
+//#pragma comment(lib,"boost_program_options-vc140-mt-gd-1_58.lib")
+#pragma comment(lib,"boost_regex-vc140-mt-gd-1_58.lib")
+//#pragma comment(lib,"boost_filesystem-vc140-mt-gd-1_58.lib")
+
+
+
 
 //using namespace tf2_ros;
 
