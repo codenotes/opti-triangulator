ba66f661d537f88d9799ea66d4dc5c08d629fcad
codenotes <gbrill@infusion.com>
2015-07-15 22:48:04 -0400

    adde some readme, have skelton of service caller ready to test.

--- opti_triangulator/beaconLocationClient/beaconLocationClient.cpp
+++ opti_triangulator/beaconLocationClient/beaconLocationClient.cpp
@@ -9,6 +9,7 @@
 
 using namespace triangulator;
 triangulator::beaconSettings gBeaconSettings;
+bool gGotReading = false;
 
 
 void beaconCallback(const triangulator::beaconSettings & msg)
@@ -16,11 +17,14 @@ void beaconCallback(const triangulator::beaconSettings & msg)
 	
 
 	gBeaconSettings = msg;
+	gGotReading = true;
 
 
 
 //	ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
+
+enum TriangulatorDSCommands { GET_LOCALIZATION = 6, GET_VOLTAGE = 3, SET_ENV_LIMITS = 15, SET_BEACON_LIST = 16 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -30,12 +34,25 @@ int _tmain(int argc, _TCHAR* argv[])
 
 
 	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("beaconPublish", 1000, beaconCallback);
-
+	//ros::Subscriber sub = n.subscribe("beaconPublish", 1000, beaconCallback);
+	std::string topic = "beaconPublish";
+	triangulator::beaconSettingsConstPtr p=ros::topic::waitForMessage<triangulator::beaconSettings>(topic);
+	//
 	ros::ServiceClient client = n.serviceClient<triangulator::triangulatorCommand>("triangulatorCmd");//this is important as it is the name of the service we are looking for
 	triangulator::triangulatorCommand srv;
 
 	
+	srv.request.triCommand.bSettings.beaconValues = p->beaconValues;
+	srv.request.triCommand.commandID = TriangulatorDSCommands::SET_BEACON_LIST;
+
+	if (client.call(srv))
+	{
+	
+	}
+	else
+	{
+
+	}
 
 	ros::spin();
 
